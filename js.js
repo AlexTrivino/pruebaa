e2c0e56a95f8fc8151546e7b8f31334458f5b140
Joycesolis12 <144949189+Joycesolis12@users.noreply.github.com>
2025-07-22 01:37:28 -0500

    Update js.js
    se actualizo la funcion de mostrar datos

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-const enep = 8
+function mostrarDato() {
+  const datos = [
+    "¡Puedes estar con un pie en el hemisferio norte y otro en el sur al mismo tiempo!",
+    "La línea equinoccial no pasa exactamente por el monumento, sino a unos 240 metros.",
+    "En este lugar, los antiguos pueblos indígenas ya sabían del equinoccio antes de la llegada de los europeos.",
+    "Aquí puedes ver cómo el agua gira en sentidos opuestos dependiendo del lado de la línea."
+  ];
+
+  const indice = Math.floor(Math.random() * datos.length);
+  document.getElementById("dato").innerText = datos[indice];
+}
